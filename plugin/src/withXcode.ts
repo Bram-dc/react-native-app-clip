a4191fe914b425af65833b98ff0206e53150b08a
Bram del Canho <bramdelcanho@gmail.com>
2025-12-15 13:19:21 +0100

    fix(withXcode): remove unnecessary type assertion for projectName

--- plugin/src/withXcode.ts
+++ plugin/src/withXcode.ts
@@ -46,7 +46,7 @@ export const withXcode: ConfigPlugin<{
     addTargetDependency(xcodeProject, target);
 
     addPbxGroup(xcodeProject, {
-      projectName: projectName as string,
+      projectName: projectName,
       targetName,
       platformProjectRoot,
     });
